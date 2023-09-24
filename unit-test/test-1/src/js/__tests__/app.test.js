cab0688065fdfb6c4ed78246786c2ffaff5d577c
Ruslan <rus.ilin1979@gmail.com>
2023-09-24 12:59:11 +0500

    dz_test-ci

---
+++ unit-test/test-1/src/js/__tests__/app.test.js
@@ -0,0 +1,18 @@
+import healthIndicator from '../app';
+
+test.each([
+    ['healthy', { name: 'Маг', health: 100 }, 'healthy'],
+    ['healthy', { name: 'Маг', health: 51 }, 'healthy'],
+    ['wounded', { name: 'Маг', health: 50 }, 'wounded'],
+    ['wounded', { name: 'Маг', health: 15 }, 'wounded'],
+    ['wounded', { name: 'Маг', health: 33 }, 'wounded'],
+    ['critical', { name: 'Маг', health: 14 }, 'critical'],
+    ['critical', { name: 'Маг', health: 3 }, 'critical'],
+    ['critical', { name: 'Маг', health: 0 }, 'critical'],
+])(
+    ('must show the level of health - %s'),
+    (level, obj, expected) => {
+        const result = healthIndicator(obj);
+        expect(result).toBe(expected);
+    },
+);
---
+++ unit-test/test-1/src/js/app.js
@@ -0,0 +1,12 @@
+export default function healthIndicator(params) {
+    let result = '';
+    if (params.health > 50) {
+        result = 'healthy';
+    } else if (params.health >= 15) {
+        result = 'wounded';
+    } else {
+        result = 'critical';
+    }
+    return result;
+}
+console.log(healthIndicator({ name: 'Маг', health: 90 }));
