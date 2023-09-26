83376c4fd871dfc2eba38946d174532930a34453
Ruslan <rus.ilin1979@gmail.com>
2023-09-26 19:32:24 +0500

    dz_regex

---
+++ regex/regex-1/src/js/validator.js
@@ -0,0 +1,8 @@
+export default class Validator {
+    static validateUsername(name) {
+        const forbiddenCharacters = /[^(a-zA-Z\d-_)]/.test(name);
+        const threeNumbers = /\d{3,}/.test(name);
+        const doesNotStartAndEndWithLetters = /^[-_\d]|[-_\d]$/.test(name);
+        return !forbiddenCharacters && !threeNumbers && !doesNotStartAndEndWithLetters;
+    }
+}
